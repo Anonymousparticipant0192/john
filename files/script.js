7f96a635a62aadf70df04fc3b56ad1164b054781
Anonymousparticipant0192 <161688441+Anonymousparticipant0192@users.noreply.github.com>
2024-02-29 19:15:02 -0800

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".", {
+    strings: ["Grade 11 student Programming"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
